891341792c1da5d95aaf3932e24d265d1be8b5f6
Gaurav-Soni24 <sonigaurav2021@gmail.com>
2025-02-17 16:20:35 +0530

    my commit

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 
 const allowedOrigins = [
-    "http://localhost:5173",  // Local Development
+    "http://localhost:5173/",  // Local Development
     "https://vishalalfredtask.vercel.app" // Production Frontend on Vercel
 ];
 
